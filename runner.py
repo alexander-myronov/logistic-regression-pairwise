268994eb26ca429defd50463f4e6956150ce6780
Alexander Myronov <alexander.myronov@gmail.com>
2017-02-07 00:48:18 +0100

    pool fix

--- runner.py
+++ runner.py
@@ -108,7 +108,8 @@ def perform_grid_search(estimator, X, y, scorer, param_grid, n_outer_folds,
                         n_outer_test_size,
                         n_inner_folds,
                         n_inner_test_size,
-                        base_folder):
+                        base_folder,
+                        mapper=itertools.imap):
     """
     returns
     test score for each outer fold
@@ -126,7 +127,7 @@ def perform_grid_search(estimator, X, y, scorer, param_grid, n_outer_folds,
                                    test_size=n_outer_test_size,
                                    random_state=42).split(X=X, y=y)
 
-    mapper = mp.Pool(processes=4).imap_unordered
+
     # mapper = itertools.imap
 
     for n_fold, (train_index, test_index) in enumerate(folds):
@@ -196,7 +197,7 @@ def get_estimator_descritpion(estimator):
 # In[ ]:
 
 def test_models(estimators, estimator_grids, X, Y, scorer, n_outer_folds, n_inner_folds,
-                n_outer_test_size, n_inner_test_size, base_dir):
+                n_outer_test_size, n_inner_test_size, base_dir, mapper=itertools.imap):
     estimator_scores = []
     # estimator_scores_std = np.zeros(len(estimators))
     assert len(estimators) <= len(estimator_grids)
@@ -212,7 +213,8 @@ def test_models(estimators, estimator_grids, X, Y, scorer, n_outer_folds, n_inne
                                                 n_inner_folds=n_inner_folds,
                                                 n_inner_test_size=n_inner_test_size,
                                                 n_outer_test_size=n_outer_test_size,
-                                                base_folder='%s/%s' % (base_dir, name))
+                                                base_folder='%s/%s' % (base_dir, name),
+                                                mapper=mapper)
         print(scores_test)
         estimator_scores.append(scores_test)
 
@@ -295,7 +297,7 @@ def accuracy_scorer(estimator, X, y):
 
 
 def prepare_and_train(name, X, y, estimator_index, n_inner_folds=3, n_inner_test_size=0.2,
-                      n_outer_folds=3, n_outer_test_size=0.2):
+                      n_outer_folds=3, n_outer_test_size=0.2, mapper=itertools.imap):
     print(name, get_estimator_descritpion(estimators[estimator_index]))
 
     try:
@@ -311,7 +313,8 @@ def prepare_and_train(name, X, y, estimator_index, n_inner_folds=3, n_inner_test
                                         n_outer_test_size=n_outer_test_size,
                                         n_inner_folds=n_inner_folds,
                                         n_inner_test_size=n_inner_test_size,
-                                        base_dir='data/%s' % name)
+                                        base_dir='data/%s' % name,
+                                        mapper=mapper)
         # pr.disable()
 
         # ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
@@ -444,8 +447,8 @@ if __name__ == '__main__':
         pool.close()
     else:
 
-        #
-
+        pool = mp.Pool()
+        mapper = pool.imap_unordered
         for (name, (dataset)), estimator_index in itertools.product(datasets.items()[::1],
                                                                     xrange(0, len(estimators))):
             value = \
@@ -469,7 +472,9 @@ if __name__ == '__main__':
                                                n_inner_test_size=args.gs_test_size,
                                                n_outer_folds=args.cv_folds,
                                                n_outer_test_size=args.cv_test_size,
+                                               mapper=mapper
                                                ))
+        pool.close()
 
     end_time = time.time()
 
