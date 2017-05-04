31cc0daf02d6370ff6a9cf767a2538ff630d51d9
Alexander Myronov <alexander.myronov@gmail.com>
2017-05-04 04:40:57 +0200

    a notebook to convert csv files from chemia.zip into .libsvm

--- start_sensitivity.py
+++ start_sensitivity.py
@@ -212,7 +212,7 @@ def get_max_percents(y, disjoint_labels_links):
     :return: percent_labels, percent_links, percent_unlabeled
     """
     if disjoint_labels_links:
-        return 0.3, 0.3, 0.4
+        return 0.4, 0.4, 0.4
     else:
         return 0.5, 0.5, 0.5
 
